1e1f290a7f9669c27705732ea9cf256d30ffa7e6
obscure723 <obscure723@yone-lab.com>
2018-09-15 11:13:47 +0800

    no message

--- recom/dbconnect.php
+++ recom/dbconnect.php
@@ -8,8 +8,10 @@
 // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // $dbh->query('SET NAMES utf8');
 
-$dbh=pg_connect("host=ec2-107-21-233-72.compute-1.amazonaws.com dbname=d4ie600704rlj2 user=bzkssfwiizyfbd password=302b20c30e09b7b80aa750ca8c8de6493fa390841971f5554021eda866b6c572");
+// $dbh=pg_connect("host=ec2-107-21-233-72.compute-1.amazonaws.com dbname=d4ie600704rlj2 user=bzkssfwiizyfbd password=302b20c30e09b7b80aa750ca8c8de6493fa390841971f5554021eda866b6c572");
 
+$conn_string = "host=ec2-107-21-233-72.compute-1.amazonaws.com port=5432 dbname=d4ie600704rlj2 user=bzkssfwiizyfbd password=302b20c30e09b7b80aa750ca8c8de6493fa390841971f5554021eda866b6c572";
+$dbconn4 = pg_connect($conn_string);
 // require '../vendor/autoload.php';
 // \Cloudinary::config(array(
 //     "cloud_name" => "hb2ekbyxa",
