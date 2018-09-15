3e2ad022394c24b0455c71d6baeb9148939ad633
obscure723 <obscure723@yone-lab.com>
2018-09-15 11:11:42 +0800

    no message

--- recom/dbconnect.php
+++ recom/dbconnect.php
@@ -10,7 +10,7 @@
 
 $dbh=pg_connect("host=ec2-107-21-233-72.compute-1.amazonaws.com dbname=d4ie600704rlj2 user=bzkssfwiizyfbd password=302b20c30e09b7b80aa750ca8c8de6493fa390841971f5554021eda866b6c572");
 
-require '../vendor/autoload.php';
+// require '../vendor/autoload.php';
 // \Cloudinary::config(array(
 //     "cloud_name" => "hb2ekbyxa",
 //     "api_key" => "935614219356682",
